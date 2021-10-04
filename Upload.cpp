bc998d96de22793d8fc105540a1f153ac9515cf8
DanielMsi <88250649+DanielMsi@users.noreply.github.com>
2021-10-04 19:21:42 +0300

    Add files via upload

---
+++ Upload.cpp
@@ -0,0 +1,23 @@
+#include "cstring"
+
+#include "Upload.h"
+#include "TCPServer.h"
+
+void Upload::execute(Knn& knn, int client_sock) {
+    string output="Please upload your local train CSV file.";
+    TCPServer::sendMessage(output, client_sock);
+
+    string input=TCPServer::readMessage(client_sock);
+    knn.setTrain(input);
+
+    output="Please upload your local test CSV file.";
+    TCPServer::sendMessage(output, client_sock);
+
+    input=TCPServer::readMessage(client_sock);
+    knn.setTest(input);
+
+    output="Upload complete";
+    TCPServer::sendMessage(output, client_sock);
+
+    TCPServer::readMessage(client_sock);
+}
