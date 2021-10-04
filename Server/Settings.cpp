3f929c1acd76f2d8d5b2663f3d48eab3470260b1
DanielMsi <88250649+DanielMsi@users.noreply.github.com>
2021-10-04 19:22:53 +0300

    Add files via upload

--- Server/Settings.cpp
+++ Server/Settings.cpp
@@ -11,9 +11,9 @@ void Settings::execute(Knn &knn, int client_sock) {
                     + ", distance metric = " + knn.getDistance();
     TCPServer::sendMessage(output, client_sock);
 
-    string buffer = TCPServer::readMessage(client_sock);
-    char input[4096];
-    strcpy(input, buffer.c_str());
+    string input = TCPServer::readMessage(client_sock);
+    char buffer[4096];
+    strcpy(buffer, input.c_str());
 
     std::vector<string> strings = Iris::strToVector(input, ' ');
 
@@ -22,29 +22,26 @@ void Settings::execute(Knn &knn, int client_sock) {
         continueInput = false;
     }
     while (continueInput) {
-        string isDouble = strtok(input, ".");
+        string isDouble = strtok(buffer, ".");
         // Let's assume we only enter a number and then a string.
         int newK = stoi(strings[0]);
         string newType = strings[1];
         if (newK > 10 || newK < 1 || isDouble != input) {
             output = "Invalid value for K";
             TCPServer::sendMessage(output, client_sock);
-            buffer = TCPServer::readMessage(client_sock);
-            strcpy(input, buffer.c_str());
-            strings = Iris::strToVector(input, ' ');
+            input = TCPServer::readMessage(client_sock);
         } else if (newType != "EUC" && newType != "MAN" && newType != "CHE") {
             output = "Invalid value for distance type";
             TCPServer::sendMessage(output, client_sock);
-            buffer = TCPServer::readMessage(client_sock);
-            strcpy(input, buffer.c_str());
-            strings = Iris::strToVector(input, ' ');
+            input = TCPServer::readMessage(client_sock);
         } else {
             knn.setK(newK);
             knn.setDistanceType(newType);
             continueInput = false;
+            TCPServer::sendMessage("Great success", client_sock);
+            TCPServer::readMessage(client_sock);
         }
+        strcpy(buffer, input.c_str());
+        strings = Iris::strToVector(input, ' ');
     }
-    TCPServer::sendMessage("Great success", client_sock);
-
-    TCPServer::readMessage(client_sock);
 }
