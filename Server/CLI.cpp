2bc0edfdd6a32f8edc7714f38f877c2cf076317b
DanielMsi <88250649+DanielMsi@users.noreply.github.com>
2021-10-12 19:14:28 +0300

    Add files via upload

--- Server/CLI.cpp
+++ Server/CLI.cpp
@@ -15,41 +15,43 @@ int TCPServer::theSocket = 0;
 
 void CLI::start() {
     TCPServer::createSocket();
-    int client_sock = TCPServer::acceptSocket();
-    if (client_sock < 0) {
-        close(client_sock);
-        return;
-    }
-    std::vector<Command *> commands = {new Upload(), new Settings(), new Classify(), new Display(), new Download(),
-                                       new Matrix()};
-    std::vector<string> list;
-    list.emplace_back("Welcome to the KNN Classifier Server. Please choose an option:");
-    int counter = 1;
-    for (Command *command:commands) {
-        list.push_back(to_string(counter) + "." + "\t" + command->getDescription());
-        ++counter;
-    }
-    list.push_back(to_string(counter) + "." + "\t" + "exit");
-    string output = Iris::vectorToStr(list, '$');
-    Knn knn = Knn();
+    while (true){
+        int client_sock = TCPServer::acceptSocket();
+        if (client_sock < 0) {
+            close(client_sock);
+            return;
+        }
+        std::vector<Command *> commands = {new Upload(), new Settings(), new Classify(), new Display(), new Download(),
+                                           new Matrix()};
+        std::vector<string> list;
+        list.emplace_back("Welcome to the KNN Classifier Server. Please choose an option:");
+        int counter = 1;
+        for (Command *command:commands) {
+            list.push_back(to_string(counter) + "." + "\t" + command->getDescription());
+            ++counter;
+        }
+        list.push_back(to_string(counter) + "." + "\t" + "exit");
+        string output = Iris::vectorToStr(list, '$');
+        Knn knn = Knn();
 
-    while (true) {
-        TCPServer::sendMessage(output, client_sock);
+        while (true) {
+            TCPServer::sendMessage(output, client_sock);
 
-        string input = TCPServer::readMessage(client_sock);
-        try {
-            if (stoi(input) > 7 || stoi(input) < 1) {
-                TCPServer::sendMessage("Wrong number", client_sock);
+            string input = TCPServer::readMessage(client_sock);
+            try {
+                if (stoi(input) > 7 || stoi(input) < 1) {
+                    TCPServer::sendMessage("Wrong number", client_sock);
+                    TCPServer::readMessage(client_sock);
+                } else if (input == "7") {
+                    close(client_sock);
+                    break;
+                } else {
+                    commands[stoi(input) - 1]->execute(knn, client_sock);
+                }
+            } catch (exception e) {
+                TCPServer::sendMessage("Wrong choice", client_sock);
                 TCPServer::readMessage(client_sock);
-            } else if (input == "7") {
-                close(client_sock);
-                exit(1);
-            } else {
-                commands[stoi(input) - 1]->execute(knn, client_sock);
             }
-        } catch (exception e) {
-            TCPServer::sendMessage("Wrong choice", client_sock);
-            TCPServer::readMessage(client_sock);
         }
     }
 }
